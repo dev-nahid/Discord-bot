06dc93a2fc85ea894b0c8fff2a304af8cbd4f8d4
Nahid Hasan <15887143+dev-nahid@users.noreply.github.com>
2022-08-02 13:10:32 +0600

    Myfirst bot setup on my discord server

---
+++ scr/mybot.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const { REST, Routes } = require('discord.js');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+// const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+// (async () => {
+//   try {
+//     console.log('Started refreshing application (/) commands.');
+
+//     await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+
+//     console.log('Successfully reloaded application (/) commands.');
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', (message) => {
+    console.log(message.content);
+});
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
